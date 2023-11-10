01dcf8e2feb9f25e14e1f85263fa8577867d0a9d
Luis A. Gonzalez <luisalbertoag21@gmail.com>
2023-11-10 14:30:06 +0100

    DOM

--- UT04/JS/PR0406.js
+++ UT04/JS/PR0406.js
@@ -4,11 +4,9 @@ let div = document.getElementsByClassName('images')
 button.addEventListener('click', () => {
     let image = document.getElementsByTagName('img'); 
       
-    let aux = (Math.random()*10/2).toFixed();
+    let aux = Math.trunc( Math.random()*5 );      // 0.0 y 4.99999  // 0 y 4
     console.log(aux);
-    if(aux ==5){
-        aux = 4;
-    }
+
     div[0].append(image[aux])
 })
 console.log(div);
