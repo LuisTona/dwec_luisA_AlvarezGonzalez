6da98d375f5e7f9a733e8afaa213845a96784b29
Luis A. Gonzalez <luisalbertoag21@gmail.com>
2023-12-05 12:52:57 +0100

    T6

---
+++ UT06/JS/PR0604.js
@@ -0,0 +1,29 @@
+
+let div = document.getElementById('content');
+
+let cont = 0;
+
+function renderList(){
+    
+    div.innerHTML = ``;
+    let h1 = document.createElement('h1');
+    h1.textContent = cont;
+    div.append(h1);
+    
+    let clicker = document.createElement('div');
+    clicker.className = 'clicker';
+    
+    clicker.addEventListener('click', (event)=>{
+        cont++;
+        renderList(); 
+    })
+    let top = Math.random()*(100 -10)+10;
+    let left = Math.random()*(100 -10)+10;
+    clicker.style.width = left+'px';
+    clicker.style.height = top+'px';
+    clicker.style.top = Math.random()*(window.innerHeight - top-50)+'px';
+    clicker.style.left = Math.random()*(window.innerWidth - left)+'px';
+    console.log(top);
+    div.append(clicker);
+}
+renderList();
