d9200e4390206ac3fbc3c160af5256c35c656a83
Luis A. Gonzalez <luisalbertoag21@gmail.com>
2023-12-19 10:35:30 +0100

    T7

--- UT07/JS/script.js
+++ UT07/JS/script.js
@@ -2,6 +2,7 @@ let form = document.forms.register;
 
 let mail = form.elements.correo;
 let pass = form.elements.pass;
+// let repPass = form.element.pass2;
 
 mail.addEventListener('blur', ()=>{
     let email = mail.value.split("")
@@ -18,40 +19,79 @@ mail.addEventListener('blur', ()=>{
     }
     // console.log(email.indexOf("@"));
 })
-let cont = 0;
+
 let pwd = [];
-let level = 0;    
+let level = 0; 
+
 function isUpper(palabra){
     level = 0;
+    let especiales = '@#~€¬·$%&/()=_.<>+-*?¿¡!|ªº';
     palabra.forEach(e =>{
-        level = level + 1;
-        return e === e.toUpperCase();
+        especiales.split('').forEach(element => {
+            palabra == element ? level = level + 1 :'';
+        });
+        e === e.toUpperCase() ? level = level + 1 : '';
     })
 }
 
-function specialCharacter(letra){
-    let especiales = '@#~€¬·$%&/()=_.<>+-*?¿¡!|ªº';
-    especiales.split('').forEach(e => {
-        return letra == e;
-    });
-    // return letra ===
-}
-
 pass.addEventListener('keydown', (e)=>{
-    console.log(`tecla pulsada: ${e.key}`);
+    // console.log(`tecla pulsada: ${e.key}`);
 
     if(e.key == 'Backspace'){
-        pwd.splice(cont, 1);
-        cont = cont -1;
-    }else if(e.key =='ArrowRight' || e.key == 'ArrowUp' || e.key == 'ArrowLeft' || e.key == 'ArrowDown' || e.key == 'Shift'){
+        if(pass.value.length >= 0){
+            pwd.splice(pass.value.length, 1);
+            isUpper(pwd);
+        }else{
+            level = 0;
+        }
 
     }else{
-        pwd[cont] = e.key;
-        cont = cont +1;
+        
+        pwd[pass.value.length] = e.key;
         isUpper(pwd);
-        console.log(level); 
+        level =(pwd.toString().replaceAll(',', ' ').length + level) / 4;
+
     }
+    
+    
+    let control = document.getElementById('control');
+    switch(level){
+        case 0:
+        case 1:
+        case 2:       
+        console.log(level);
+            control.style.width = 10+'px';
+            control.style.backgroundColor = 'red'
+        break;
+
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            console.log(level);
+            control.style.width = 50+'px';
+            control.style.backgroundColor = 'orange'
+            break;
+        case 7:
+        case 8:
+        case 9:
+            console.log(level);
+            control.style.width = 100+'px';
+            control.style.backgroundColor = 'green'
+            break;
+        case 10:
+        case 11:
+        case 12:
+            console.log(level);
+            control.style.width = 150+'px';
+            control.style.backgroundColor = 'lime'
+            break;
+    }
+
 })
-console.log(mail);
 
+// pass2.addEventListener('blur', ()=>{
 
+// })
+// }else if(e.key =='ArrowRight' || e.key == 'ArrowUp' || e.key == 'ArrowLeft' || e.key == 'ArrowDown' || e.key == 'Shift' || e.key == 'AltGraph' || e.key == 'Control'){
+// console.log(mail);
